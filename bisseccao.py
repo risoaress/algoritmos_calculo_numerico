26389123f16f2fb6438d9c3cce21f890f1401245
Ricardo Soares <38105787+risoaress@users.noreply.github.com>
2025-03-22 18:36:35 -0300

    Update bisseccao.py
    Adicionada exceção para entradas ambas positivas e ambas negativas.

--- bisseccao.py
+++ bisseccao.py
@@ -1,6 +1,6 @@
 # implementação simples do método da bissecção em Python
 
-f = lambda x: x**2 - 1
+f = lambda x: x**2 - 1 # altere a função desejada aqui
 
 print("Digite um ponto x com f(x) < 0:")
 a = float(input())
@@ -12,12 +12,17 @@ mid = (a + b)/2
 e = 6
 zero = 10**(-e)
 
-while(abs(f(mid)) > zero): # a comparação é com um valor não-nulo para evitar problemas de precisão numérica
+while(abs(f(mid)) > zero): # a comparação é uma inequação para evitar problemas de precisão numérica. Trata-se de uma medição de proximidade ao zero real.
     if f(mid) < 0:
         a = mid
     else:
         b = mid
     
     mid = (a + b)/2
+
+    if f(a)*f(b) > 0:
+        print("Não foi possível encontrar o zero da função com os pontos especificados")
+        break
+
     
 print("o ponto do zero é: %.6f" % mid)
